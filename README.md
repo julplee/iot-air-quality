5b2ef62066f31ea494f125013e7d2949708d6e5a
Julien Plée <julplee@gmail.com>
2019-07-28 18:44:05 +0200

    Now sends tweets when launched

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Code that handles IOT for Air Quality monitoring with Raspberry Pi
 
 ## Prerequisites
-`pip3 install pyserial adafruit-io`
+`pip3 install pyserial adafruit-io twython`
 
 ## Launch the project
 `python3 air-quality.py`
--- air-quality.py
+++ air-quality.py
@@ -1,15 +1,26 @@
 import serial, time
 from Adafruit_IO import Client
+from twython import Twython
+
+TWITTER_APP_KEY = 'YOUR_APP_KEY'
+TWITTER_APP_SECRET = 'YOUR_APP_SECRET'
+TWITTER_OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN'
+TWITTER_OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET'
 
 ADAFRUIT_IO_USERNAME = 'YOUR_AIO_USERNAME'
 ADAFRUIT_IO_KEY = 'YOUR_AIO_KEY'
 
-# Create an instance of the REST client.
+# Create an instance of the adafruit REST client.
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
+# Create an instance of the Twitter client.
+twitter = Twython(TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_TOKEN_SECRET)
+
+# Create an instance of the serial manager of SDS011
 ser = serial.Serial('/dev/ttyUSB0')
 
-while True:
+# Send the recorded value to Adafruit IO
+def sendAdafruit(): 
 	data = []
 	for index in range(0,10):
 		datum = ser.read()
@@ -19,4 +30,20 @@ while True:
 	aio.send('kingswoodtwofive', pmtwofive)
 	pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 	aio.send('kingswoodten', pmten)
-	time.sleep(10)
+
+	return pmtwofive, pmten
+
+# Send a new status with value of PM2.5 and PM10
+def sendTweet(pm25, pm10):
+    twitter.update_status(status='For now, there are ' + str(pm25) + 'µg/m3 of PM2.5 and ' + str(pm10) + 'µg/m3 of PM10')
+
+def main():
+	pm25, pm10 = sendAdafruit()
+	sendTweet(pm25, pm10)
+
+	while True:
+		sendAdafruit()
+		time.sleep(10)
+	
+if __name__ == '__main__':
+    main()
