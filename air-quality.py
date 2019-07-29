413940fb161e7c114fae9f6f3d0ffed25626ead7
Julien Plée <julplee@gmail.com>
2019-07-29 18:13:04 +0200

    Optimize code readability

--- air-quality.py
+++ air-quality.py
@@ -19,30 +19,33 @@ twitter = Twython(TWITTER_APP_KEY, TWITTER_APP_SECRET, TWITTER_OAUTH_TOKEN, TWIT
 # Create an instance of the serial manager of SDS011
 ser = serial.Serial('/dev/ttyUSB0')
 
-# Send the recorded value to Adafruit IO
-def sendAdafruit(): 
+def takeMeasure():
 	data = []
 	for index in range(0,10):
 		datum = ser.read()
 		data.append(datum)
 
-	pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-	aio.send('kingswoodtwofive', pmtwofive)
-	pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-	aio.send('kingswoodten', pmten)
+	pm25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
+	pm10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
-	return pmtwofive, pmten
+	return pm25, pm10
+
+# Send the recorded value to Adafruit IO
+def sendAdafruit(pm25, pm10): 
+	aio.send('kingswoodtwofive', pm25)
+	aio.send('kingswoodten', pm10)
 
 # Send a new status with value of PM2.5 and PM10
 def sendTweet(pm25, pm10):
-    twitter.update_status(status='For now, there are ' + str(pm25) + 'µg/m3 of PM2.5 and ' + str(pm10) + 'µg/m3 of PM10')
+    twitter.update_status(status='For now, there are ' + str(pm25) + ' µg/m3 of PM2.5 and ' + str(pm10) + ' µg/m3 of PM10')
 
 def main():
-	pm25, pm10 = sendAdafruit()
+	pm25, pm10 = takeMeasure()
 	sendTweet(pm25, pm10)
 
 	while True:
-		sendAdafruit()
+		pm25, pm10 = takeMeasure()
+		sendAdafruit(pm25, pm10)
 		time.sleep(10)
 	
 if __name__ == '__main__':
